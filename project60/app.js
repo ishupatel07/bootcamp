44bfb26072d68d405d20110b3f02f8437c5be14f
Ishwar Patel <ishwarrpatel22@gmail.com>
2024-03-26 16:35:52 +0530

    assignment complete

---
+++ project60/app.js
@@ -0,0 +1,11 @@
+let sentance = "how are you?"
+let check  = sentance.endsWith("?")
+console.log(check)
+
+
+let ab = "jr. dev jr. dev jr. dev"
+
+let replace = ab.replaceAll("jr. dev" , "software enginner")
+console.log(replace)
+
+
