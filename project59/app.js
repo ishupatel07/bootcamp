eb26d6aa9e8444cf056f0834d6292f15551fe656
Ishwar Patel <ishwarrpatel22@gmail.com>
2024-03-26 15:17:42 +0530

    assignment complete

---
+++ project59/app.js
@@ -0,0 +1,17 @@
+let mouse = {}
+mouse.brand = "steelseries"
+mouse.color = "redblack"
+mouse.use = "gaming"
+mouse.cabel = "wire"
+
+mouse.smotthness = function () {
+    console.log("too Smooth");
+}
+
+mouse.warrenty = function () {
+    console.log("5 years");
+}
+
+mouse.sold = function () {
+    console.log("paid my debt");
+}
