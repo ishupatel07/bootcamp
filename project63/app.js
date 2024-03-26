e05cfc6f0df64220a8e40d3a676a2bbaee6db631
Ishwar Patel <ishwarrpatel22@gmail.com>
2024-03-26 17:38:52 +0530

    assignment complete

--- project63/app.js
+++ project63/app.js
@@ -1,1 +1,15 @@
-const 
+document.querySelector("button").addEventListener("click", getDrink)
+
+function getDrink(){
+    let drinkChoice = document.querySelector("input").value
+    const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+drinkChoice
+   
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(`error ${err}`);
+    })
+}
