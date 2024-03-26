034f5a0291b70a65eaf1e75fdd7abd4cd06f235a
Ishwar Patel <ishwarrpatel22@gmail.com>
2024-03-26 14:59:22 +0530

    assignment 2 complete

--- project58/app.js
+++ project58/app.js
@@ -3,3 +3,9 @@ let sum = arr.reduce((a,b) => a+b)
 console.log(sum);
 
 
+function arrN(arr) {
+    let newArr = [...arr];
+    return newArr.map((a) => a * a);
+}
+
+console.log(arrN([1, 2, 3, 4, 5, 6]));
