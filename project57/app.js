ca88104dd8135d5164c2aa0ab09aa558adcb7613
Ishwar Patel <ishwarrpatel22@gmail.com>
2024-03-26 14:23:45 +0530

    assignment complete

--- project57/app.js
+++ project57/app.js
@@ -4,3 +4,16 @@ console.log(drink)
 let word = "hello world"
 word.includes("apple")
 console.log(word)
+
+function spc(){
+    let rn =  Math.floor(Math.random() * 3)
+    if(rn === Number(0)){
+        return "stone"
+    }else if( rn === Number(1)){
+        return "paper"
+    }else if(rn  === Number(2)){
+        return "scissor"
+    }
+}
+
+console.log(spc())
