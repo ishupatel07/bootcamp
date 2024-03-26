6cca2874f313cc5a02a9b9a254a7338610e4572a
Ishwar Patel <ishwarrpatel22@gmail.com>
2024-03-26 15:02:20 +0530

    assignment 3 complete

--- project58/app.js
+++ project58/app.js
@@ -9,3 +9,12 @@ function arrN(arr) {
 }
 
 console.log(arrN([1, 2, 3, 4, 5, 6]));
+
+function reverseTheString(str){
+    let reverse = str.split("").reverse().join("")
+    return reverse
+}
+
+console.log(reverseTheString("abcde"))
+
+
