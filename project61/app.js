3a700186ee715684d6cec1ebc3e23256a16171b6
Ishwar Patel <ishwarrpatel22@gmail.com>
2024-03-26 16:44:11 +0530

    assigment complete

--- project61/app.js
+++ project61/app.js
@@ -2,3 +2,10 @@ let positiveAffirmation  = "When God Here whom you fear you? huh. "
 
 console.log(positiveAffirmation.repeat(4))
 
+let arr = ["k","r","i","s","h","n","a"]
+let arr2 = ["r","a","d","h","e"]
+
+let arr3 = [...arr2, ...arr]
+
+console.log(arr3.join(""))
+
